7f5347a73efc27f00eec6c100765b2c46dfd70f5
lloydnoone <lloydnoone92@gmail.com>
2019-09-28 22:21:33 +0100

    hardcode array for deploy

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   //array used for saving data
   let wallIndex = []
@@ -323,7 +322,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function loadWalls() {
-    const wallsData = JSON.parse(localStorage.getItem('wallsData'))
+    //const wallsData = JSON.parse(localStorage.getItem('wallsData'))
+    //have to hardcode it for online deploy
+    const wallsData = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,28,31,39,40,42,43,44,45,46,48,51,53,54,55,56,57,59,60,62,63,64,65,66,68,71,73,74,75,76,77,79,80,82,83,84,85,86,88,91,93,94,95,96,97,99,100,119,120,122,123,124,125,126,128,129,130,131,133,134,135,136,137,139,140,142,143,144,145,146,149,150,153,154,155,156,157,159,160,167,172,179,180,181,182,183,184,185,187,189,190,192,194,195,196,197,198,199,209,210,220,221,222,223,224,225,227,232,234,235,236,237,238,239,240,241,242,243,244,245,247,248,249,250,251,252,254,255,256,257,258,259,260,268,269,270,271,279,280,282,283,284,286,288,289,290,291,293,295,296,297,299,300,302,303,304,306,313,315,316,317,319,320,326,327,328,329,330,331,332,333,339,340,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,359,360,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399]
     //upon load cycle cells
     for (let i = 0; i < cells.length; i++) {
       // at each cell, check to see if the index matches any in the saved walls array
@@ -335,7 +336,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function loadPills() {
-    const pillsData = JSON.parse(localStorage.getItem('pillsData'))
+    //const pillsData = JSON.parse(localStorage.getItem('pillsData'))
+    // have to hardcode for online deploy
+    const pillsData = [21,22,23,24,25,26,27,29,30,32,33,34,35,36,37,38,41,47,49,50,52,58,67,69,70,72,81,87,89,90,92,98,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,121,138,141,158,161,162,163,164,165,166,173,174,175,176,177,178,186,193,226,233,246,253,261,262,263,264,265,266,267,272,273,274,275,276,277,278,285,287,292,294,301,305,307,308,309,310,311,312,314,318,321,322,323,324,325,334,335,336,337,338,341,358,361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378]
     totalPills = pillsData.length
     //upon load cycle cells
     for (let i = 0; i < cells.length; i++) {
@@ -348,7 +351,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function loadEnergizer() {
-    const energizerData = JSON.parse(localStorage.getItem('energizerData'))
+    //const energizerData = JSON.parse(localStorage.getItem('energizerData'))
+    //have to hardcode for online deploy
+    const energizerData = [61,78,281,298]
     //upon load cycle cells
     for (let i = 0; i < cells.length; i++) {
       // at each cell, check to see if the index matches any in the saved walls array
